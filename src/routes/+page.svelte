35cf1c3b429f91050de1ec4622ecef3a30f255f1
Grellheist <grellheist@tutanota.com>
2023-08-09 12:51:32 -0300

    finished navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 	export let data;
-    import "../app.css"
+	import '../app.css';
 </script>
+
+<nav class="select-none w-full flex justify-between items-center h-28 z-50">
+	<a href="/">
+		<h1 class="text-lg pl-4 lg:pl-8">Finance Website</h1>
+	</a>
+	<div class="flex gap-12">
+		<a href="/" class=""> Placeholder </a>
+		<a href="/" class=""> Placeholder </a>
+		<a href="/" class=""> Placeholder </a>
+		<a href="/" class=""> Placeholder </a>
+	</div>
+	<div class="pr-8 gap-12 flex items-center">
+		<a href="/">Placeholder</a>
+		<a href="/" class="rounded-lg ease-in-out duration-300 font-semibold py-3 px-6 text-white bg-green-700 hover:bg-green-800">Placeholder</a>
+	</div>
+</nav>
 
 <p class="bg-black text-sky-500">
 	{#await data.streamed.views}

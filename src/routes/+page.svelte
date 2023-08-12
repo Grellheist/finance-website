11fd38fe14a4373c49ee8b57bd27b9dd91f80695
Grellheist <grellheist@tutanota.com>
2023-08-12 18:14:38 -0300

    i can't do this rn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,3 +31,30 @@
 		delectus laborum, necessitatibus, ab omnis! Dignissimos.
 	</p>
 </div>
+<div class="flex flex-col gap-12 items-center justify-center h-screen p-12" id="section-2">
+	<h1 class="text-4xl font-extrabold text-green-700">section 2</h1>
+	<p>Finance is really important, here is why:</p>
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dicta quos magnam aliquam
+		pariatur asperiores rerum blanditiis, incidunt velit provident rem soluta alias inventore
+		delectus laborum, necessitatibus, ab omnis! Dignissimos.
+	</p>
+</div>
+<div class="flex flex-col gap-12 items-center justify-center h-screen p-12" id="section-3">
+	<h1 class="text-4xl font-extrabold text-green-700">section 3</h1>
+	<p>Finance is really important, here is why:</p>
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dicta quos magnam aliquam
+		pariatur asperiores rerum blanditiis, incidunt velit provident rem soluta alias inventore
+		delectus laborum, necessitatibus, ab omnis! Dignissimos.
+	</p>
+</div>
+<div class="flex flex-col gap-12 items-center justify-center h-screen p-12" id="section-4">
+	<h1 class="text-4xl font-extrabold text-green-700">section 4</h1>
+	<p>Finance is really important, here is why:</p>
+	<p>
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dicta quos magnam aliquam
+		pariatur asperiores rerum blanditiis, incidunt velit provident rem soluta alias inventore
+		delectus laborum, necessitatibus, ab omnis! Dignissimos.
+	</p>
+</div>

d19407d1b1547739f21b2cedf106ffa5c8df9ada
Grellheist <grellheist@tutanota.com>
2023-08-19 18:15:02 -0300

    ye

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,6 @@
 	<div class="flex flex-col lg:flex-row gap-12 items-center">
 		<img src="/images/graph-2.jpg" alt="" class="w-[640px] rounded" />
 		<div class="flex-col gap-12 flex items-center">
-			<p>Finance is really important, here is why:</p>
 			<p>
 				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem dicta quos magnam aliquam
 				pariatur asperiores rerum blanditiis, incidunt velit provident rem soluta alias inventore
@@ -35,14 +34,12 @@
 </div>
 <div class="flex flex-col gap-12 items-center justify-center h-screen p-12 bg-[#EDFDF2]" id="reviews">
 	<h1 class="text-4xl font-extrabold text-green-700">What people think of us</h1>
-	<p>Finance is really important, here is why:</p>
 	<p>
 		reviews
 	</p>
 </div>
 <div class="flex flex-col gap-12 items-center justify-center h-screen p-12 bg-[#FEFDF9]" id="prices">
 	<h1 class="text-4xl font-extrabold text-green-700">Our prices</h1>
-	<p>Finance is really important, here is why:</p>
 	<p>
 		prices
 	</p>

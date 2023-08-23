ea26d74220140e16272565b6043226d99ecd7847
Grellheist <grellheist@tutanota.com>
2023-08-23 13:01:18 -0300

    whoops

--- src/components/Reviews.svelte
+++ src/components/Reviews.svelte
@@ -4,7 +4,7 @@
 
 <div class="bg-green-200 p-8 rounded-xl space-y-12 max-w-[300px] shadow-xl hover:scale-105 transition duration-300">
 	<img {src} alt="User's face" class="h-20 mx-auto rounded-full" loading="lazy" decoding="async" />
-	<h4 class="uppercase text-xl font-bold">{name}</h4>
+	<h3 class="uppercase text-xl font-bold">{name}</h3>
 	<p class="text-md leading-7 my-3 font-light">
 		{testimonial}
 	</p>
